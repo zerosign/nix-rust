f1308c0129cc6e37b713cdb4625db728ae6f2dc2
Carl Lerche <me@carllerche.com>
2015-02-21 00:29:15 +0000

    rebuild pages at cd64f6d

---
+++ implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['nix'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='nix/errno/enum.Errno.html' title='nix::errno::Errno'>Errno</a>","<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='nix/errno/enum.Errno.html' title='nix::errno::Errno'>Errno</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
